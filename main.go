d80263028fb7c39cf31fa55b14d62cc14a1e68f7
valentin-cochin <cochin.valentin@hotmail.fr>
2021-10-26 00:09:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
 	port := ":4000"
 	log.Println("Starting server on ", port)
 	err := http.ListenAndServe(port, mux)
@@ -17,4 +20,12 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving request: ", r.URL.Path)
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
