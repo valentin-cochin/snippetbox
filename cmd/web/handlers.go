2eafe6e4e34e9b3a9b4665a3a55f4ed25198b409
valentin-cochin <cochin.valentin@hotmail.fr>
2021-10-29 00:23:42 +0200

    Update templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,20 +14,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	// Include the footer partial in the template files.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents any
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 	if err != nil {
 		log.Println(err.Error())
