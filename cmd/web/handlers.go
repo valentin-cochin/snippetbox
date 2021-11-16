d207a2981c819cabd1327737ad1a4cd6406ae180
valentin-cochin <cochin.valentin@hotmail.fr>
2021-11-16 13:49:29 +0100

    Improve flash after creation of the snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -42,15 +42,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the PopString() method to retrieve the value for the "flash" key.
-	// PopString() also deletes the key and value from the session data, so it
-	// acts like a one-time fetch. If there is no matching key in the session
-	// data this will return the empty string.
-	flash := app.session.PopString(r, "flash")
-
-	// Pass the flash message to the template.
 	app.render(w, r, "show.page.tmpl", &templateData{
-		Flash:   flash,
 		Snippet: s,
 	})
 }
