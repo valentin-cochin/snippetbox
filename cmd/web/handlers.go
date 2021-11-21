8e99412adf7684fdfbfb4a02c5eada6f70c6d34c
valentin-cochin <cochin.valentin@hotmail.fr>
2021-11-21 22:38:22 +0100

    User signup with encryption

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -89,14 +89,12 @@ func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
-	// Parse the form data.
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// Validate the form contents using the form helper we made earlier.
 	form := forms.New(r.PostForm)
 	form.Required("name", "email", "password")
 	form.MaxLength("name", 255)
@@ -104,14 +102,28 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 	form.MatchesPattern("email", forms.EmailRX)
 	form.MinLength("password", 10)
 
-	// If there are any errors, redisplay the signup form.
 	if !form.Valid() {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	// Otherwise send a placeholder response (for now!).
-	fmt.Fprintln(w, "Create a new user...")
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address is already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
