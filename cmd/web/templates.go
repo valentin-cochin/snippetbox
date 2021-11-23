5930ebb77cda6be4f97d11fba10ba129a55d1dae
valentin-cochin <cochin.valentin@hotmail.fr>
2021-11-23 13:07:48 +0100

    Hide some elements when user not authentificated

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -10,11 +10,12 @@ import (
 )
 
 type templateData struct {
-	CurrentYear int
-	Flash       string
-	Form        *forms.Form
-	Snippet     *models.Snippet
-	Snippets    []*models.Snippet
+	CurrentYear     int
+	Flash           string
+	Form            *forms.Form
+	IsAuthenticated bool
+	Snippet         *models.Snippet
+	Snippets        []*models.Snippet
 }
 
 // Create a humanDate function which returns a nicely formatted string
